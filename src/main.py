879fb5af77d31fe8f1b20d7f296b55ed4b9bdf45
André Conde <andre.conde@aluno.ifsp.edu.br>
2024-03-28 11:14:33 -0300

    refactor: improve col_dda

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import json
 from io import StringIO
+from math import ceil
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,8 +13,31 @@ from mappers import BaseMapper
 CORRELATION_MATRIX_SIZE = 20
 
 
-def data_preprocessing(data: pd.DataFrame):
-    data = data.dropna()
+def data_preprocessing(data: pd.DataFrame, method: str):
+    """
+    Preprocesses the given DataFrame based on the specified method.
+
+    Parameters:
+        data (pd.DataFrame): The input DataFrame to be preprocessed.
+        method (str): The method to be used for preprocessing. Valid options are "clean", "mean", "mode", and "median".
+
+    Returns:
+        pd.DataFrame: The preprocessed DataFrame.
+
+    Raises:
+        ValueError: If an invalid method is provided.
+    """
+    match method:
+        case "clean":
+            data = data.dropna()
+        case "mean":
+            data = data.fillna(data.mean())
+        case "mode":
+            data = data.fillna(data.mode().iloc[0])
+        case "median":
+            data = data.fillna(data.median())
+        case _:
+            raise ValueError("Invalid method")
     return data
 
 
@@ -49,26 +73,23 @@ def show_information_data_frame(data: pd.DataFrame, correl_matrix: bool = False)
         plot_correlation_matrix(mrx)
 
 
-def VisualizePcaProjection(df, targetColumn):
+def VisualizePcaProjection(finalDf, targetColumn):
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel("Principal Component 1", fontsize=15)
     ax.set_ylabel("Principal Component 2", fontsize=15)
     ax.set_title("2 component PCA", fontsize=20)
-    targets = [
-        0,
-        1,
-    ]
+    targets = {0: "no", 1: "yes"}
     colors = ["r", "g"]
     for target, color in zip(targets, colors):
-        indicesToKeep = df[targetColumn] == target
+        indicesToKeep = finalDf[targetColumn] == target
         ax.scatter(
-            df.loc[indicesToKeep, "principal component 1"],
-            df.loc[indicesToKeep, "principal component 2"],
+            finalDf.loc[indicesToKeep, "principal component 1"],
+            finalDf.loc[indicesToKeep, "principal component 2"],
             c=color,
             s=50,
         )
-    ax.legend(targets)
+    ax.legend(targets.values())
     ax.grid()
     plt.show()
 
@@ -88,29 +109,65 @@ def pca(x_score, df: pd.DataFrame, target):
     VisualizePcaProjection(finalDf, "y")
 
 
-def col_dda(df: pd.DataFrame, col: str, target: str, graph: str):
+def col_dda(df: pd.DataFrame, col: str | list[str], target: str, graph: str, **kwargs):
     """
     This function receives a dataframe, a column name, a target column name and a graph type.
     It plots the distribution of the column values and the target values.
     """
-    if graph == "hist":
+    match graph:
+        case "hist":
+            if df[col].dtype == "object":
+                df_copy = BaseMapper.get_mapper(col).revert_list(df[col])
+                df[col] = df_copy
 
-        df[col].hist()
-        plt.title(f"Distribution of {col}")
-        plt.xlabel(col)
-        plt.ylabel("Frequency")
-        plt.show()
-    elif graph == "box":
-        df.boxplot(column=col, by=target)
-        plt.title(f"Distribution of {col} by {target}")
-        plt.xlabel(target)
-        plt.ylabel(col)
-        plt.show()
-    else:
-        raise ValueError("Invalid graph type")
+            df[col].hist()
+            plt.title(f"Distribution of {col}")
+            plt.xlabel(col)
+            plt.ylabel("Frequency")
+            plt.show()
+        case "box":
+            df.boxplot(column=col, by=target)
+            plt.title(f"Distribution of {col} by {target}")
+            plt.xlabel(target)
+            plt.ylabel(col)
+            plt.show()
+        case "bar":
+            if kwargs.get("bins"):
+                df[col].value_counts(bins=kwargs.get("bins"), sort=False).plot(
+                    kind="bar", rot=0
+                )
+            else:
+                df[col].value_counts().plot(kind="bar")
+            plt.title(f"Bar plot of {col}")
+            plt.xlabel(col)
+            plt.ylabel("Frequency")
+            plt.show()
+        case "pie":
+            df_copy = BaseMapper.get_mapper(col).revert_list(df[col])
+            df[col] = df_copy
+            df[col].value_counts().plot(kind="pie", autopct="%1.1f%%")
+            plt.title(f"Pie plot of {col}")
+            plt.ylabel(col)
+            plt.show()
+        case "density":
+            df[col].value_counts(sort=False).plot(kind="density")
+            plt.title(f"Count plot of {col}")
+            plt.xlabel(col)
+            plt.ylabel("Frequency")
+            plt.show()
+        case "scatter":
+            df_copy = BaseMapper.get_mapper(col).revert_list(df[col])
+            df[col] = df_copy
+            plt.scatter(df[col], df[target])
+            plt.title(f"Scatter plot of {col} and {target}")
+            plt.xlabel(col)
+            plt.ylabel(target)
+            plt.show()
+        case _:  # default case
+            raise ValueError("Invalid graph type")
 
 
-INPUT_FILE = "./data/bank-additional/bank-additional/bank-additional-full.csv"
+INPUT_FILE = "./src/data/bank-additional/bank-additional/bank-additional-full.csv"
 NA_VALUE = "unknown"
 
 
@@ -133,7 +190,7 @@ def main():
     """
 
     # load the entry columns from data/entry.json
-    io = StringIO(open("./data/entry.json", "r").read())
+    io = StringIO(open("./src/data/entry.json", "r").read())
     entry = json.load(io)
 
     # the columns to be used in the analysis
@@ -157,11 +214,13 @@ def main():
         na_values=NA_VALUE,  # the missing values
         sep=";",  # the separator of the file
         usecols=names,  # the columns to be used in the analysis
-        converters={col: _make_map_adapter(col) for col in categorical},  # the converters
+        converters={
+            col: _make_map_adapter(col) for col in categorical
+        },  # the converters
     )
 
-    # perform data preprocessing - drop missing values
-    df = data_preprocessing(df)
+    # perform data preprocessing
+    df = data_preprocessing(df, "mode")
 
     # getting the features to be used in the analysis
     x = df.loc[:, features].values
@@ -177,13 +236,19 @@ def main():
         [normalized_df, df[target]], axis=1  # axis = 0 for rows, axis = 1 for columns
     )
 
+    # show_information_data_frame(df, correl_matrix=False)
     # show_information_data_frame(normalized_df, correl_matrix=True)
+    show_information_data_frame(normalized_df, correl_matrix=False)
 
     # PCA projection
     # pca(x_score, df, target)
 
     # Plot the dda of cols
-    col_dda(df, "marital", "y", "hist")
+    # col_dda(df, "age", "y", "bar", bins=ceil(df["age"].max() / 10))
+    # col_dda(df, "job", "y", "pie")
+    # col_dda(df, "education", "y", "pie")
+    # col_dda(df, "marital", "y", "pie")
+    col_dda(df, "marital", "y", "box")
 
 
 if __name__ == "__main__":
